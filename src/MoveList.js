f8de3246078925dbe7392d9a6d30d28cf2dc8f4d
Ryan Aldous <aldous.ryan1@gmail.com>
2022-06-25 18:05:25 -0400

    changes to move font

--- src/MoveList.js
+++ src/MoveList.js
@@ -7,29 +7,29 @@ function Move(name, type, damageModifier, strongAgainst, effect) {
 }
 
 /*Bulbasaur Moves */
-const VineWhip = new Move('Vine Whip', 'Grass', 0.7, ['Water', 'Rock', 'Ground']);
-const LeechSeed = new Move('Leech Seed', 'Grass', 0.5, ['Water', 'Rock', 'Ground'], 'Heal');
-const RazorLeaf = new Move('Razor Leaf', 'Grass', 0.8, ['Water', 'Rock', 'Ground'], 'Critical');
-const Absorb = new Move('Absorb', 'Grass', 0.8, ['Water', 'Rock', 'Ground'], 'Heal')
+const VineWhip = new Move('VINE WHIP', 'Grass', 0.7, ['Water', 'Rock', 'Ground']);
+const LeechSeed = new Move('LEECH SEED', 'Grass', 0.5, ['Water', 'Rock', 'Ground'], 'Heal');
+const RazorLeaf = new Move('RAZOR LEAF', 'Grass', 0.8, ['Water', 'Rock', 'Ground'], 'Critical');
+const Absorb = new Move('ABSORB', 'Grass', 0.8, ['Water', 'Rock', 'Ground'], 'Heal')
 
 /* Charmander Moves */
-const Scratch = new Move('Scratch', 'Normal', 1.1, []);
-const Ember = new Move('Ember', 'Fire', 0.7, ['Bug', 'Grass', 'Ice'], 'Burn');
-const Slash = new Move('Slash', 'Normal', 1.3, [])
-const Flamethrower = new Move('Flamethrower', 'Fire', 1.0, ['Bug', 'Grass', 'Ice']);
-const FireFang = new Move('Fire Fang', 'Fire', 0.9, ['Bug', 'Grass', 'Ice'], 'Burn');
+const Scratch = new Move('SCRATCH', 'Normal', 1.1, []);
+const Ember = new Move('EMBER', 'Fire', 0.7, ['Bug', 'Grass', 'Ice'], 'Burn');
+const Slash = new Move('SLASH', 'Normal', 1.3, [])
+const Flamethrower = new Move('FLAMETHROWER', 'Fire', 1.0, ['Bug', 'Grass', 'Ice']);
+const FireFang = new Move('FIRE FANG', 'Fire', 0.9, ['Bug', 'Grass', 'Ice'], 'Burn');
 
 /* Squirtle Moves */
-const Bubble = new Move('Bubble', 'Water', 0.8, ['Rock', 'Fire', 'Ground']);
-const RapidSpin = new Move('Rapid Spin', 'Normal', 1.1, []);
-const Protect = new Move('Protect', 'Normal', 0.0, [], 'DefenseUp');
-const WaterPulse = new Move('Water Pulse', 'Water', 0.9, ['Rock', 'Fire', 'Ground'])
+const Bubble = new Move('BUBBLE', 'Water', 0.8, ['Rock', 'Fire', 'Ground']);
+const RapidSpin = new Move('RAPID SPIN', 'Normal', 1.1, []);
+const Protect = new Move('PROTECT', 'Normal', 0.0, [], 'DefenseUp');
+const WaterPulse = new Move('WATER PULSE', 'Water', 0.9, ['Rock', 'Fire', 'Ground'])
 
 /* Shared Moves */
 const None = new Move('---')
-const Tackle = new Move('Tackle', 'Normal', 1.0, []);
-const TakeDown = new Move('Take Down', 'Normal', 1.2, []);
-const Growl = new Move('Growl', 'Normal', 0.0, [])
+const Tackle = new Move('TACKLE', 'Normal', 1.0, []);
+const TakeDown = new Move('TAKE DOWN', 'Normal', 1.2, []);
+const Growl = new Move('GROWL', 'Normal', 0.0, [])
 
 
 const BulbasaurMoveList = [/* LV5  */[Tackle, None, None, Growl],
